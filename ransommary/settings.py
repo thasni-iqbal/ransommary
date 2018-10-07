7c4329a069f26eac8869ac69eeb61d618dda20a6
thasni-iqbal <thasni@souqalmal.com>
2018-10-07 23:10:34 +0400

    updated database connection

--- ransommary/settings.py
+++ ransommary/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'NAME': 'ransommary',
         'HOST': '127.0.0.1',
         'PORT': '3306',
-        'USER': 'root',
+        'USER': 'ransommary',
         'PASSWORD': 'sam16Rocks',
     }
 }
