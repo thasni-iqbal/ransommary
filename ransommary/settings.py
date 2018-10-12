fde60cc6a7dd91271bebeedd55597a42ea791a1b
thasni5390 <30571211+thasni5390@users.noreply.github.com>
2018-10-12 13:30:32 +0400

    Update settings.py

--- ransommary/settings.py
+++ ransommary/settings.py
@@ -77,12 +77,12 @@ WSGI_APPLICATION = 'ransommary.wsgi.application'
 
 DATABASES = {
         'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'ransommary',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
         'USER': 'ransommary',
-        'PASSWORD': 'sam16Rocks',
+        'PASSWORD': 'samRocks',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
