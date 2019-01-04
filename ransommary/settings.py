faf0fd3c41d26935a0ecf0c345a7bf4c01c490e8
thasni-iqabl <thasni@souqalmal.com>
2019-01-04 20:20:07 +0400

    added new host

--- ransommary/settings.py
+++ ransommary/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '7rgte3rk@66x@a(*=f53ce6t6k#9!wd0wb8e=s1p3n@$dcs9xs'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.241.137.114', 'ransommary.co.uk', 'www.ransommary.co.uk', 'localhost']
+ALLOWED_HOSTS = ['192.241.137.114', 'ransommary.co.uk', 'www.ransommary.co.uk', 'localhost', 'jsnetwork.co.uk', 'www.jsnetwork.co.uk']
 
 
 # Application definition
@@ -82,7 +82,7 @@ DATABASES = {
         'USER': 'ransommary',
         'PASSWORD': 'samRocks',
         'HOST': 'localhost',
-        'PORT': '',
+        'PORT': '5432',
     }
 }
 
