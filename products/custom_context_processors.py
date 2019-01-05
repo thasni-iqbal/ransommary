1de21a50f0b548a80501fa54ca75764b8453a9c2
thasni-iqabl <thasni@souqalmal.com>
2019-01-05 23:50:04 +0400

    correction

--- products/custom_context_processors.py
+++ products/custom_context_processors.py
@@ -3,7 +3,7 @@ from .site_config import site_config
 
 def hostname(request):
     hostname = request.META['HTTP_HOST']
-    config = site_config['default'];
+    config = site_config['default']
     if 'jsnetwork' in hostname:
         config = site_config['jsnetwork']
     return {
